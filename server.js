3db2356fa54629f68dae0c41d22073479bdac6a2
yozo37 <yohannzororpro@gmail.com>
2023-11-17 16:01:08 +0100

    aajout delete

--- server.js
+++ server.js
@@ -7,28 +7,32 @@ const bcrypt = require('bcrypt');
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    database:process.env.DB_DATA ,
-    user:process.env.DB_USER ,
-    password:process.env.DB_PWD ,
+    database: process.env.DB_DATA,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
 });
+
 app.use(cors());
 
 app.get('/api/jeu/', async (req, res) => {
-    console.log("lancement de la connexion");
-    const conn = await pool.getConnection();
-    console.log("lancement de la requete");
-    const rows = await conn.query("SELECT * FROM jeu");
-    console.log(rows);
-    res.status(200).json(rows);
+    try {
+        console.log("lancement de la connexion");
+        const conn = await pool.getConnection();
+        console.log("lancement de la requete");
+        const rows = await conn.query("SELECT * FROM jeu");
+        console.log(rows);
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.get('/api/recherche/:nom', async (req, res) => {
-    console.log("Lancement de la connexion");
-    const conn = await pool.getConnection();
-    console.log("Lancement de la requête");
-    
     try {
-        // Utilisez le nom du jeu comme paramètre dans la requête SQL
+        console.log("Lancement de la connexion");
+        const conn = await pool.getConnection();
+        console.log("Lancement de la requête");
         const rows = await conn.query("SELECT * FROM jeu WHERE Nom_jeu = ?", [req.params.nom]);
         console.log(rows);
         res.status(200).json(rows);
@@ -36,32 +40,41 @@ app.get('/api/recherche/:nom', async (req, res) => {
         console.error("Erreur lors de l'exécution de la requête :", err);
         res.status(500).send("Erreur serveur");
     } finally {
-        // N'oubliez pas de libérer la connexion une fois la requête terminée
         conn.release();
     }
 });
 
 app.get('/api/utilisateurs/', async (req, res) => {
-    console.log("lancement de la connexion");
-    const conn = await pool.getConnection();
-    console.log("lancement de la requete");
-    const rows = await conn.query("SELECT * FROM utilisateurs");
-    console.log(rows);
-    res.status(200).json(rows);
+    try {
+        console.log("lancement de la connexion");
+        const conn = await pool.getConnection();
+        console.log("lancement de la requete");
+        const rows = await conn.query("SELECT * FROM utilisateurs");
+        console.log(rows);
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
+
 app.get('/api/utilisateurs/:id', async (req, res) => {
-    console.log("lancement de la connexion");
-    const conn = await pool.getConnection();
-    console.log("lancement de la requete");
-    const rows = await conn.query("SELECT * FROM Utilisateurs WHERE ID_Utilisateur = ?", [req.params.id]);
-    console.log(rows);
-    res.status(200).json(rows);
-})
+    try {
+        console.log("lancement de la connexion");
+        const conn = await pool.getConnection();
+        console.log("lancement de la requete");
+        const rows = await conn.query("SELECT * FROM Utilisateurs WHERE ID_Utilisateur = ?", [req.params.id]);
+        console.log(rows);
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 app.post('/api/utilisateurs/', async (req, res) => {
-    console.log(req.body);
-
     try {
+        console.log(req.body);
         const { Nom_Utilisateur, Email, Mot_de_passe } = req.body;
 
         if (!Nom_Utilisateur || !Email || !Mot_de_passe) {
@@ -75,11 +88,9 @@ app.post('/api/utilisateurs/', async (req, res) => {
 
         const IDUtilisateur = Number(result.insertId);
 
-        await conn.release();
-
         res.status(201).json({ ID_Utilisateur: IDUtilisateur, message: 'User created successfully.' });
-    } catch (err) {
-        console.error(err);
+    } catch (error) {
+        console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
@@ -93,6 +104,7 @@ app.post('/api/login', async (req, res) => {
         }
 
         const conn = await pool.getConnection();
+
         try {
             const result = await conn.query('SELECT * FROM Utilisateurs WHERE Nom_Utilisateur = ?', [Nom_Utilisateur]);
 
@@ -103,6 +115,7 @@ app.post('/api/login', async (req, res) => {
             }
 
             const user = result[0];
+            console.log('User Object:', user);
             console.log('Entered Password:', Mot_de_passe);
             console.log('Stored Hashed Password:', user.Mot_de_passe);
 
@@ -111,6 +124,10 @@ app.post('/api/login', async (req, res) => {
             console.log('Password Comparison Result:', isPasswordValid);
 
             if (!isPasswordValid) {
+                console.error('Password comparison failed. Details:', {
+                    enteredPassword: Mot_de_passe,
+                    storedPassword: user.Mot_de_passe,
+                });
                 return res.status(401).json({ error: 'Invalid Nom_Utilisateur or Mot_de_passe.' });
             }
 
@@ -123,34 +140,107 @@ app.post('/api/login', async (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 });
+
+
 app.get('/api/locations/', async (req, res) => {
-    console.log("lancement de la connexion");
-    const conn = await pool.getConnection();
-    console.log("lancement de la requete");
-    const rows = await conn.query("SELECT * FROM locations");
-    console.log(rows);
-    res.status(200).json(rows);
+    try {
+        console.log("lancement de la connexion");
+        const conn = await pool.getConnection();
+        console.log("lancement de la requete");
+        const rows = await conn.query("SELECT * FROM locations");
+        console.log(rows);
+        res.status(200).json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 });
-// app.post('/api/locations/:id', async (req, res) => {
-//     try {
-//       // Supposons que vous recevez ces données du front-end
-//       const { ID_Utilisateur, ID_Jeu, Note, Commentaire } = req.body;
-  
-//       // Ajoutez ces données à la table locations dans la base de données
-//       await pool.query(
-//         'INSERT INTO locations (ID_Utilisateur, ID_Jeu, Note, Commentaire) VALUES (?, ?, ?, ?)',
-//         [ID_Utilisateur, ID_Jeu, Note, Commentaire]
-//       );
-  
-//       res.status(201).json({ message: 'Jeu loué avec succès' });
-//     } catch (error) {
-//       console.error('Erreur lors de la location du jeu :', error);
-//       res.status(500).json({ message: 'Erreur lors de la location du jeu' });
-//     }
-//   });
-  
+app.delete('/api/utilisateurs/:id', async (req, res) => {
+    let conn
+    let id = req.params.id
+    console.log(id);
+    try {
+        console.log("lancement de la connexion")
+        conn = await pool.getConnection();
+        console.log("lancement de la requête DELETE")
+        const result = await conn.query("DELETE * FROM Utilisateurs WHERE ID_Utilisateur = ?", [req.params.id]);
+        res.status(200).json({ message: 'utilisateur bien supprimée !' })
+
+    }
+    catch (err) {
+        res.status(400).json({ message: 'Erreur requête lors de la suppression !' })
+    }
+    finally {
+        if (conn) {
+            conn.release(); // Libérer la connexion à la base de données.
+        }
+    }
+})
+app.delete('/api/locations/:id', async (req, res) => {
+    let conn
+    let id = req.params.id
+    console.log(id);
+    try {
+        console.log("lancement de la connexion")
+        conn = await pool.getConnection();
+        console.log("lancement de la requête DELETE")
+        const result = await conn.query("DELETE * FROM Utilisateurs WHERE ID_locations = ?", [req.params.id]);
+        res.status(200).json({ message: 'locations bien supprimée !' })
+
+    }
+    catch (err) {
+        res.status(400).json({ message: 'Erreur requête lors de la suppression !' })
+    }
+    finally {
+        if (conn) {
+            conn.release(); // Libérer la connexion à la base de données.
+        }
+    }
+})
+app.delete('/api/locations/:id', async (req, res) => {
+    let conn
+    let id = req.params.id
+    console.log(id);
+    try {
+        console.log("lancement de la connexion")
+        conn = await pool.getConnection();
+        console.log("lancement de la requête DELETE")
+        const result = await conn.query("DELETE * FROM Utilisateurs WHERE ID_locations = ?", [req.params.id]);
+        res.status(200).json({ message: 'locations bien supprimée !' })
+
+    }
+    catch (err) {
+        res.status(400).json({ message: 'Erreur requête lors de la suppression !' })
+    }
+    finally {
+        if (conn) {
+            conn.release(); // Libérer la connexion à la base de données.
+        }
+    }
+})
+app.delete('/api/jeu/:id', async (req, res) => {
+    let conn
+    let id = req.params.id
+    console.log(id);
+    try {
+        console.log("lancement de la connexion")
+        conn = await pool.getConnection();
+        console.log("lancement de la requête DELETE")
+        const result = await conn.query("DELETE * FROM Utilisateurs WHERE ID_jeu = ?", [req.params.id]);
+        res.status(200).json({ message: 'jeu bien supprimée !' })
+
+    }
+    catch (err) {
+        res.status(400).json({ message: 'Erreur requête lors de la suppression !' })
+    }
+    finally {
+        if (conn) {
+            conn.release(); // Libérer la connexion à la base de données.
+        }
+    }
+})
 
 
 app.listen(3000, () => {
-    console.log("Serveur a l'ecoute");
+    console.log("Serveur a l'ecoute ") ;
 });
